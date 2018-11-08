c8c4910089671d40feb895b7641ec118b6c84ca8
lukeludlow <lukeludlow@live.com>
2018-11-08 12:52:43 -0700

    rm cgi-bin

--- cgi-bin/alexa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Cache-Control: no-cache, no-store, must-revalidate";
-echo "Pragma: no-cache";
-echo "Expires: 0";
-echo "Content-Type: application/json;charset=UTF-8"
-echo ""
-echo "{ \"version\": \"1.0\", \"response\": { \"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"Got it. Yes, your command was received.\"} }}"
