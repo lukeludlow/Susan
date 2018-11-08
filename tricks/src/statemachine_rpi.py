4a11c675599eb7ec38dc778492f8a870f64cb23d
lukeludlow <lukeludlow@live.com>
2018-11-08 13:12:42 -0700

    statemachine package ready to test

--- tricks/src/statemachine_rpi.py
+++ tricks/src/statemachine_rpi.py
@@ -5,15 +5,17 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty, EmptyResponse, Trigger, TriggerResponse
 from rover_msgs.msg import WaypointNav, NavStatus
 from wheel_controller import WheelController
+# trick modules
+import xboxarm
+import shake
+import nod
 
 class StateMachine:
     def __init__(self):
         rospy.init_node('state_machine')
         self.rate = rospy.Rate(60)  # set rate to 60 hz
         # publishers
-        self.pub_drive = rospy.Publisher('/auto_vel', Twist, queue_size=10)
-        self.status_timer = rospy.Timer(rospy.Duration(.1), self.statusPubCallback)
-        rospy.loginfo('state machine running')
+        rospy.loginfo('state machine')
 
     def execute(self):
         while not rospy.is_shutdown():
@@ -50,37 +52,34 @@ class StateMachine:
 
 if __name__ == '__main__':
     rospy.init_node('automaton')
-    rospy.loginfo('##################')
-    rosply.loginfo('automaton running')
-    rospy.loginfo('##################')
-    self.rate = rospy.Rate(60)  # set rate to 60 hz
-    # publishers
-    self.pub_drive = rospy.Publisher('/auto_vel', Twist, queue_size=10)
-    self.status_timer = rospy.Timer(rospy.Duration(.1), self.statusPubCallback)
-    
-    SM = StateMachine()
+    rospy.loginfo('###')
+    rosply.loginfo('### automaton running!')
+    rospy.loginfo('###')
 
-    rate = rospy.Rate(60)  # set rate to 60 hz
+    # trick objects    
+    SM = StateMachine()
+    xboxarm = Arm_XBOX()
 
     # execute automaton
+    rate = rospy.Rate(60)  # set rate to 60 hz
     while not rospy.is_shutdown():
         # Start when Xbox controller recognized
-        if len(xbox.joy.buttons) > 0 and xbox.trigger_check():
-            if not xbox.ready_msg:
+        if len(xboxarm.joy.buttons) > 0 and xboxarm.trigger_check():
+            if not xboxarm.ready_msg:
                 rospy.loginfo('Arm Controller Ready')
-                xbox.ready_msg = True
+                xboxarm.ready_msg = True
             # every time check toggle of state
-            xbox.check_method()
+            xboxarm.check_method()
             # check for kill switch (True = Killed)
-            if xbox.state.kill  == False:
+            if xboxarm.state.kill  == False:
                 # call appropriate function for state
                 # defaults to JointControl
-                if xbox.state.mode == 'JointControl':
-                    xbox.joint_cmd()
-                elif xbox.state.mode == 'IK Arm - Base,Tool':
-                    xbox.arm_IK_base_tool()
-                elif xbox.state.mode == 'IK Arm - Tool,Tool':
-                    xbox.arm_IK_tool_tool()
+                if xboxarm.state.mode == 'JointControl':
+                    xboxarm.joint_cmd()
+                elif xboxarm.state.mode == 'IK Arm - Base,Tool':
+                    xboxarm.arm_IK_base_tool()
+                elif xboxarm.state.mode == 'IK Arm - Tool,Tool':
+                    xboxarm.arm_IK_tool_tool()
                 else:
-                    xbox.joint_cmd()
+                    xboxarm.joint_cmd()
         rate.sleep()
