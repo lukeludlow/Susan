f51d35caf63056f1d2190c4bc645686d36e320e0
lukeludlow <lukeludlow@live.com>
2018-11-08 14:38:04 -0700

    controller fix

--- tricks/src/Controller.py
+++ tricks/src/Controller.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tf.transformations as tr
 import rospy
 
 
@@ -19,7 +18,3 @@ class Controller:
         self.heading_offset = [0,0]     # [rad, deg]
         self.current_distance = 100  # (m)
         self.num_waypoints = 0
-
-        # These are set in navigation/param/navigation_param.yaml
-        self.W_MAX = rospy.get_param('~W_MAX') # Maximum angular velocity of rover [rad/s]
-        self.DEFAULT_V = rospy.get_param('~V_MAX') # Default forward velocity of rover [m/s]
